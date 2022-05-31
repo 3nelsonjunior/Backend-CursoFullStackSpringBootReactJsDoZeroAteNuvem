de4c715c11f4b4e7c51b57b4313559aaff309b3c
Nelson Junior <3nelsonjunior@gmail.com>
2022-05-30 22:01:49 -0300

    AULA 55 -  Implementação do Post de Usuários

--- src/main/java/com/tresn/minhasfinancas/api/controller/UsuarioController.java
+++ src/main/java/com/tresn/minhasfinancas/api/controller/UsuarioController.java
@@ -1,14 +1,42 @@
 package com.tresn.minhasfinancas.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tresn.minhasfinancas.api.dto.UsuarioDto;
+import com.tresn.minhasfinancas.exceptions.RegraNegocioException;
+import com.tresn.minhasfinancas.model.entity.Usuario;
+import com.tresn.minhasfinancas.service.UsuarioService;
+
 @RestController
+@RequestMapping("api/usuarios")
 public class UsuarioController {
 	
-	@GetMapping("/")
-	public String HelloWord() {
-		return "Hello Word!!!";
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping()
+	public ResponseEntity salvar(@RequestBody UsuarioDto dto) {
+
+		Usuario usuario = Usuario.builder()
+									.nome(dto.getNome())
+									.email(dto.getEmail())
+									.senha(dto.getSenha())
+									.build();
+		try {
+			Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
+			
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+			
+		} catch (RegraNegocioException e) {
+			return  ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
 	}
 
 }
